ebc98d445ef96f4643c4e1d196fd313fb6f54964
Null value man <102916032+PsycoPython@users.noreply.github.com>
2026-09-04 12:34:25 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
 /* =========================================================================
    1. إعدادات الخادم والـ API
    ========================================================================= */
-const API_BASE_URL = "http://localhost:5000"; // استبدله برابط الخادم الفعلي
-
+const API_BASE_URL = "http://smg.runasp.net"; 
 let targetWhatsAppNumber = "963985083231";
 
 async function apiGet(endpoint) {
